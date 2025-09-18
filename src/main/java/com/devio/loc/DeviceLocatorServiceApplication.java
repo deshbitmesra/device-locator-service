c418bb4b7b6735b5c3e1fb43c6554655282992c3
desh.bitmesra <desh.bitmesra@gmail.com>
2025-09-19 02:29:29 +0530

    first commit

---
+++ src/main/java/com/devio/loc/DeviceLocatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devio.loc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceLocatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceLocatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devio/loc/DeviceLocatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devio.loc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceLocatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
