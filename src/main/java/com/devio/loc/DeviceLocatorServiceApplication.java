2c3c7a1f3f7bec284d937bb41852c14166722f03
desh.bitmesra <desh.bitmesra@gmail.com>
2025-10-17 02:59:38 +0530

    adding new branch develop

--- src/main/java/com/devio/loc/DeviceLocatorServiceApplication.java
+++ src/main/java/com/devio/loc/DeviceLocatorServiceApplication.java
@@ -2,12 +2,22 @@ package com.devio.loc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("device")
 public class DeviceLocatorServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeviceLocatorServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String getDevice(){
+		return "Generic Device";
 	}
 
 }
